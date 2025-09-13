744db47c4013a66322ed5784bfd2f130509b0cac
Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2025-09-13 22:18:54 +0600

    nav done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import HeroSection from "./components/HeroSection/HeroSection";
 
 function App() {
   return (
     <>
-      <p>A-1</p>
+      <Header />
+      <HeroSection />
     </>
   );
 }
